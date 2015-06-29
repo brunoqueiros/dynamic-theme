42f75dfb6628762e76ecc98d3ee11ccaf433cc69
Bruno Queiros <brunoofq@gmail.com>
2015-06-29 19:27:08 -0300

    Create plugin structure

--- dynamic-theme.js
+++ dynamic-theme.js
@@ -1,3 +1,34 @@
+;(function (defaults, $, window, document, undefined) {
+  'use strict';
+
+  var pluginName = 'dynamicTheme',
+      defaults = {};
+
+  function Plugin(element, options) {
+    this.element = element;
+
+    this.options = $.extend({}, defaults, options);
+
+    this._defaults = defaults;
+    this._name = pluginName;
+
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function () {}
+  };
+
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+      }
+    });
+  };
+})(jQuery, window, document);
+
+
 'use strict';
 
 function DynamicTheme (options) {
